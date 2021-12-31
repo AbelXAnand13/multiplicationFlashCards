42e49e77df8bb16d9200dd27f521133b838ee352
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-12-31 13:47:10 -0800

    final commit1 prolly gonna be more idk

--- multiplicationFlasCards/main.cpp
+++ multiplicationFlasCards/main.cpp
@@ -1,14 +1,41 @@
-//
-//  main.cpp
-//  multiplicationFlasCards
-//
-//  Created by Abel Christy Anand on 12/31/21.
-//
+// Abel Anand
 
 #include <iostream>
+#include <vector>
+#include <time.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    vector<int> nums;
+    for (int i = 1; i <= 12; i++) {
+        nums.push_back(i);
+    }
+    // populating the vector
+    
+    srand(time(0));
+    bool quit = false;
+    int answer;
+    int correct = 0;
+    int wrong = 0;
+    cout << "This is a multiplication quiz with the tables from 1 to 12 randomly\nWhen you are done, enter 0\n";
+    
+    while (!quit) {
+    int num1 = (rand()%12)+1; //first number
+    int num2 = (rand()%12)+1; //second number
+        
+    
+        cout << nums.at(num1 - 1) << " x " << nums.at(num2 - 1) << endl;
+        cin >> answer;
+        
+        if (nums.at(num1-1) * nums.at(num2-1) == answer) {
+            correct ++;
+        }
+        else if (answer == 0) {
+            cout << "Correct answers = " << correct << endl << "Wrong Answers = " << wrong << endl;
+            quit = true;
+        }
+        else {
+            wrong ++;
+        }
+    }
 }
