ed22cfc36db64ada2fa7e315b44305bbbdc98801
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2022-02-01 22:38:16 -0800

    commit2 :=(

--- multiplicationFlasCards/main.cpp
+++ multiplicationFlasCards/main.cpp
@@ -6,36 +6,20 @@
 using namespace std;
 
 int main() {
-    vector<int> nums;
+    vector<vector<int>> nums;
+    
     for (int i = 1; i <= 12; i++) {
-        nums.push_back(i);
+        nums[i-1].push_back(i);
+        for (int j = 0; j < 12; j++) {
+            nums[j].push_back(i+i);
+        }
     }
-    // populating the vector
     
-    srand(time(0));
-    bool quit = false;
-    int answer;
-    int correct = 0;
-    int wrong = 0;
-    cout << "This is a multiplication quiz with the tables from 1 to 12 randomly\nWhen you are done, enter 0\n";
-    
-    while (!quit) {
-    int num1 = (rand()%12)+1; //first number
-    int num2 = (rand()%12)+1; //second number
-        
-    
-        cout << nums.at(num1 - 1) << " x " << nums.at(num2 - 1) << endl;
-        cin >> answer;
-        
-        if (nums.at(num1-1) * nums.at(num2-1) == answer) {
-            correct ++;
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = 0; j < nums[i].size(); j++)
+        {
+            cout << nums[i][j] << " ";
         }
-        else if (answer == 0) {
-            cout << "Correct answers = " << correct << endl << "Wrong Answers = " << wrong << endl;
-            quit = true;
-        }
-        else {
-            wrong ++;
-        }
+        cout << endl;
     }
 }
